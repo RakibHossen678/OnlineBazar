693374c42e7e96b506f299b4288add0fd4a86dba
RakibHossen678 <rh491464@gmail.com>
2024-08-13 16:15:18 +0600

    added banner section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import "./Banner.css";
+import bannerImg from "../../assets/banner.png";
+import { BsArrowRight } from "react-icons/bs";
+const Banner = () => {
+  return (
+    <header className="hero-section">
+      <section className="container">
+        <div className="banner_row">
+          <div className="hero-content">
+            <h1 className="hero-headline">Welcome to Our Store</h1>
+            <p className="hero-subheadline">
+              Discover unbeatable deals on the latest products and elevate your
+              shopping with exclusive offers. Explore our handpicked collection
+              of top-notch gadgets and stylish fashion. Don’t miss out on
+              limited-time promotions and seasonal discounts—shop now for
+              exceptional value and quality!
+            </p>
+            <button className="hero-cta">Shop Now <span><BsArrowRight /></span>
+            </button>
+          </div>
+          <div className="banner_img">
+            <img src={bannerImg} alt="" />
+          </div>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Banner;
