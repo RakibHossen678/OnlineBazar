d3127e4f16c663131a5262e74c0bd0246c971bc6
RakibHossen678 <rh491464@gmail.com>
2024-08-13 15:28:36 +0600

    added navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,64 @@
-
+import { FaCartShopping } from "react-icons/fa6";
+import "./Navbar.css";
+import { HiMiniBars3BottomRight } from "react-icons/hi2";
+import { useState } from "react";
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    
+  };
+  console.log(menuOpen)
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div className="container">
+        <div className="nav_row">
+          <h1 className="logo">
+            Online<span>Bazar</span>
+          </h1>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">Kids</a>
+            </li>
+          </ul>
+          <div>
+            <button>Login</button>
+            <p>
+              <FaCartShopping size={24} />
+            </p>
+            <p className="menubar" onClick={toggleMenu}>
+              <HiMiniBars3BottomRight size={24} />
+            </p>
+          </div>
+        </div>
+        <div className="lg_menu" style={{ display: menuOpen ? "block" : "none" }}>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">Kids</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
